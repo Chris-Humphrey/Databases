8f48bd6e98ad5f857fec3f324d4140bf7e53d16f
Chris-Humphrey <clhumphrey21@gmail.com>
2019-06-07 15:25:20 -0500

    exercise on aggregation and sorting queries

--- restaurant.sql
+++ restaurant.sql
@@ -39,6 +39,12 @@
 -- INSERT INTO restaurant VALUES (
 --     DEFAULT, 'Curly', 3, 3, 'Mexican', 'Enchiladas', TRUE, 0
 -- );
+-- INSERT INTO restaurant VALUES (
+--     DEFAULT, 'Umbrella', 3, 3, 'Chinese', 'Egg Rolls', TRUE, 0
+-- );
+-- INSERT INTO restaurant VALUES (
+--     DEFAULT, 'Dino', 3, 2, 'BBQ', 'Ribs', TRUE, 0
+-- );
 
 
 
@@ -69,4 +75,31 @@
     -- SELECT * FROM restaurant WHERE lastVisit <= 7;
 
 -- + rest haven't ate at in the last week and has 5 stars
-    SELECT * FROM restaurant WHERE lastVisit <= 7 AND stars = 5;
+    -- SELECT * FROM restaurant WHERE lastVisit <= 7 AND stars = 5;
+
+
+
+-- ++ AGGREGATION AND SORTING QUERIES ++
+
+-- + list rest closest
+    -- SELECT * FROM restaurant ORDER BY distance;
+
+-- + list top 2 rest by dist
+    -- SELECT * FROM restaurant ORDER BY distance LIMIT 2;
+
+-- + list top 2 rest by stars
+    -- SELECT * FROM restaurant ORDER BY stars DESC LIMIT 2;
+
+-- + list top 2 rest by stars where dist < 2 miles
+    -- SELECT * FROM restaurant WHERE distance < 2 ORDER BY stars DESC LIMIT 2;
+-- + count # rest in db
+    -- SELECT COUNT(*) FROM restaurant;
+
+-- + count # rest by category
+    -- SELECT COUNT(category) FROM restaurant WHERE category = 'Chinese';
+
+-- + get avg stars per rest by category
+    -- SELECT AVG(stars) FROM restaurant WHERE category = 'BBQ';
+
+-- + get max stars of rest by category
+    -- SELECT MAX(stars) FROM restaurant WHERE category = 'BBQ';
